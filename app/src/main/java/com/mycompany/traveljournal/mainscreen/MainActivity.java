d0820dd5bc4d8a1f3fa8344ea6eee920f6eadd61
Richard Pon <richard.pon@gmail.com>
2015-06-05 22:01:08 -0700

    Move Parse example code into its own class, ExampleGetUserFromParse

--- app/src/main/java/com/mycompany/traveljournal/mainscreen/MainActivity.java
+++ app/src/main/java/com/mycompany/traveljournal/mainscreen/MainActivity.java
@@ -2,16 +2,11 @@ package com.mycompany.traveljournal.mainscreen;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
 
 import com.mycompany.traveljournal.R;
 import com.mycompany.traveljournal.base.PostsListActivity;
 import com.mycompany.traveljournal.datasource.ParseClient;
-import com.mycompany.traveljournal.models.User;
-import com.parse.FindCallback;
-import com.parse.ParseException;
-
-import java.util.List;
+import com.mycompany.traveljournal.examples.ExampleGetUserFromParse;
 
 
 public class MainActivity extends PostsListActivity {
@@ -42,31 +37,7 @@ public class MainActivity extends PostsListActivity {
         super.onCreate(savedInstanceState);
 
         // Uncomment to run example of fetching record from Parse
-        //runExmaples();
+        ExampleGetUserFromParse.run();
     }
 
-    // Examples for fetching data from Parse
-    private void runExmaples() {
-        exampleGetUser();
-
-    }
-
-    /**
-     * This is an example of how to get a User with the user's id
-     */
-    private void exampleGetUser() {
-        String userId = "1";
-        User.getUserWithId(userId, new FindCallback<User>() {
-            public void done(List<User> users, ParseException e) {
-                if (e == null) {
-                    User user = users.get(0);
-                    Log.wtf(TAG, user.getName());
-                    Log.wtf(TAG, user.getUserId());
-                    Log.wtf(TAG, user.getProfileImageUrl());
-                } else {
-                    Log.wtf(TAG, "User not found");
-                }
-            }
-        });
-    }
 }
