35fdc4ca59cfad5eee9c22373638259cd3253cf0
Richard Pon <richard.pon@gmail.com>
2015-06-06 21:30:18 -0700

    Stub out getting the signed in user. Will revisit after Facebook connect

---
+++ app/src/main/java/com/mycompany/traveljournal/datasource/SignedInUser.java
@@ -0,0 +1,16 @@
+package com.mycompany.traveljournal.datasource;
+
+
+import com.mycompany.traveljournal.models.User;
+
+public class SignedInUser {
+
+    /**
+     * Returns the signed in User
+     */
+    public User getSignedInUser() {
+
+        // TODO: get the actual signed in user
+        return User.getFakeUser();
+    }
+}
