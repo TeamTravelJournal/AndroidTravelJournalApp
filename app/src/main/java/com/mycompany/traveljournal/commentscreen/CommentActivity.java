49e6b8e343e25128df3e26aaffd6073439ce5d19
Richard Pon <richard.pon@gmail.com>
2015-06-12 18:35:50 -0700

    Load comment fragment from comment activity with minimal bare layout

--- app/src/main/java/com/mycompany/traveljournal/commentscreen/CommentActivity.java
+++ app/src/main/java/com/mycompany/traveljournal/commentscreen/CommentActivity.java
@@ -1,18 +1,42 @@
 package com.mycompany.traveljournal.commentscreen;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.mycompany.traveljournal.R;
+import com.mycompany.traveljournal.datasource.ParseClient;
 
 public class CommentActivity extends ActionBarActivity {
+
+    private static final String TAG = "CommentActivity";
+    String postId;
+    ParseClient parseClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comments);
+
+        // Get Post ID
+        postId = getIntent().getStringExtra("post_id");
+
+        if (postId == null) {
+            Log.wtf(TAG, "Using default post id");
+            postId = "8nxq1SkIUo";
+        }
+
+
+        // Init Parse
+        parseClient = ParseClient.getInstance(this);
+
+        if(savedInstanceState == null) {
+            setUpFragment();
+        }
+
     }
 
 
@@ -37,4 +61,12 @@ public class CommentActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void setUpFragment() {
+        CommentFragment commentFragment =  CommentFragment.newInstance(postId);
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.flContainer, commentFragment);
+        ft.commit();
+    }
+
 }
--- app/src/main/java/com/mycompany/traveljournal/commentscreen/CommentFragment.java
+++ app/src/main/java/com/mycompany/traveljournal/commentscreen/CommentFragment.java
@@ -1,8 +1,63 @@
 package com.mycompany.traveljournal.commentscreen;
 
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mycompany.traveljournal.R;
 
 public class CommentFragment extends Fragment {
+    private final static String TAG = "CommentFragment";
+    private String postId;
+    private Toolbar toolbar;
 
+    public static CommentFragment newInstance(String postId) {
+        CommentFragment commentFragment = new CommentFragment();
+        Bundle args = new Bundle();
+        args.putString("post_id", postId);
+        commentFragment.setArguments(args);
+        return commentFragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_comment_list, container, false);
+        setUpViews(view);
+        setToolbar();
+        setUpListeners();
+        return view;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        postId = getArguments().getString("post_id", "");
+    }
+
+    public void setUpViews(View v) {
+
+    }
+
+    public void setUpListeners() {
+
+    }
+
+    private void setToolbar() {
+        if (toolbar != null) {
+            ((ActionBarActivity) getActivity()).setSupportActionBar(toolbar);
+            //toolbar.setBackgroundColor(Color.parseColor(user.getProfileBackgroundColor()));
+
+            // Set the home icon on toolbar
+            ActionBar actionbar = ((ActionBarActivity) getActivity()).getSupportActionBar();
+            actionbar.setDisplayHomeAsUpEnabled(true);
+            actionbar.setHomeAsUpIndicator(R.drawable.ic_up_menu);
+        }
+    }
 }
