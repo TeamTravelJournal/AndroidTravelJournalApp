24d79d964fd9b24b2c8e9cf4cf384bc76085aa02
esrako <esrakucukoguz@yahoo.com>
2015-06-17 19:07:34 -0700

    progres bar for create

---
+++ app/src/main/java/com/mycompany/traveljournal/common/ProgressBarListener.java
@@ -0,0 +1,10 @@
+package com.mycompany.traveljournal.common;
+
+/**
+ * Created by ekucukog on 6/16/2015.
+ */
+public interface ProgressBarListener {
+
+    void onShowProgressBar();
+    void onHideProgressBar();
+}
