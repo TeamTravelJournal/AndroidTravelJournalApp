3de3a4d5eb0d2380da65ad2731602df3a98d57bd
Srivats Jayaram <srivats666@gmail.com>
2015-06-22 17:23:54 -0700

    Added follows to profile and tab view

---
+++ app/src/main/java/com/mycompany/traveljournal/profilescreen/ProfilePagerAdapter.java
@@ -0,0 +1,62 @@
+package com.mycompany.traveljournal.profilescreen;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ImageSpan;
+
+import com.mycompany.traveljournal.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by sjayaram on 6/22/2015.
+ */
+public class ProfilePagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> mFragments = new ArrayList<>();
+    private final List<String> mFragmentTitles = new ArrayList<>();
+    private Context context;
+
+    private int[] imageResId = {
+            R.drawable.ic_magnify_white_48dp,
+            R.drawable.ic_map_marker_white_48dp,
+            R.drawable.icon_heart_white
+    };
+
+    public ProfilePagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        mFragments.add(fragment);
+        mFragmentTitles.add(title);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        /*Drawable image = context.getResources().getDrawable(imageResId[position]);
+        image.setBounds(0, 0, image.getIntrinsicWidth(), image.getIntrinsicHeight());
+        SpannableString sb = new SpannableString(" ");
+        ImageSpan imageSpan = new ImageSpan(image, ImageSpan.ALIGN_BOTTOM);
+        sb.setSpan(imageSpan, 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return sb;*/
+        return mFragmentTitles.get(position);
+    }
+}
+
