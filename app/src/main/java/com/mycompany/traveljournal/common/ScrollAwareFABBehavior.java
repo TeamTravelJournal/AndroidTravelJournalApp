944983d3c269d451c1bdac6e947c875ffa12e4df
Srivats Jayaram <srivats666@gmail.com>
2015-07-05 10:02:49 -0700

    Hide fab on timeline

--- app/src/main/java/com/mycompany/traveljournal/common/ScrollAwareFABBehavior.java
+++ app/src/main/java/com/mycompany/traveljournal/common/ScrollAwareFABBehavior.java
@@ -42,6 +42,7 @@ public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
                                final View target, final int dxConsumed, final int dyConsumed,
                                final int dxUnconsumed, final int dyUnconsumed) {
         super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+
         if (dyConsumed > 0 && !this.mIsAnimatingOut && child.getVisibility() == View.VISIBLE) {
             // User scrolled down and the FAB is currently visible -> hide the FAB
             animateOut(child);
